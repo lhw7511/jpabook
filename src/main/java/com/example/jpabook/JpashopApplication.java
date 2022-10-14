9c26d19332f10470627b95ccad705fc0bcee7e8f
lhw7511 <lhw751@gmail.com>
2022-10-14 17:20:42 +0900

    api 엔티티 직접 노출 및 dto로변환리턴

--- src/main/java/com/example/jpabook/JpashopApplication.java
+++ src/main/java/com/example/jpabook/JpashopApplication.java
@@ -2,8 +2,10 @@ package com.example.jpabook;
 
 import com.example.jpabook.domain.Address;
 import com.example.jpabook.domain.Member;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,12 @@ public class JpashopApplication {
 		Hello hello = new Hello();
 		hello.setData("TEST");
 		System.out.println(hello.getData());
+
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module(){
+		return new Hibernate5Module();
 	}
 
 }
--- src/main/java/com/example/jpabook/domain/Member.java
+++ src/main/java/com/example/jpabook/domain/Member.java
@@ -24,6 +24,7 @@ public class Member {
     private Address address;
 
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
