8847dd84cdacfdb2ba9454aab5c3d67352ef03de
lhw7511 <lhw751@gmail.com>
2022-10-20 16:49:05 +0900

    api fetch join and dto direct return

---
+++ src/main/java/com/example/jpabook/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,20 @@
+package com.example.jpabook.repository.order.simplequery;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+    private final EntityManager em;
+    public List<SimpleOrderQueryDto> findOrderDtos() {
+        return em.createQuery("select new com.example.jpabook.repository.order.simplequery.SimpleOrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)  from Order o" +
+                " join  o.member m" +
+                " join  o.delivery d", SimpleOrderQueryDto.class).getResultList();
+
+    }
+}
---
+++ src/main/java/com/example/jpabook/repository/order/simplequery/SimpleOrderQueryDto.java
@@ -0,0 +1,27 @@
+package com.example.jpabook.repository.order.simplequery;
+
+import com.example.jpabook.domain.Address;
+import com.example.jpabook.domain.Order;
+import com.example.jpabook.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class SimpleOrderQueryDto {
+
+
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+    public SimpleOrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
