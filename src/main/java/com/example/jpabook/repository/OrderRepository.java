eac76cb69962d5b1c9b9ee83af622ea4e22c81a8
lhw7511 <lhw751@gmail.com>
2022-10-27 16:59:05 +0900

    컬렉션 페이징과 한계돌파

--- src/main/java/com/example/jpabook/repository/OrderRepository.java
+++ src/main/java/com/example/jpabook/repository/OrderRepository.java
@@ -103,4 +103,12 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d",Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
